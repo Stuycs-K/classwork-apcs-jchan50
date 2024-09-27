c55236a59fc6eca0ec29b428573d5ab9e9522042
Jessie Chan <jessiechan55555@gmail.com>
2024-09-27 10:53:21 -0400

    added and tested MyArrays

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,18 @@
+public class MyArrays{
+  public static String arrayToString(int[] nums){
+    String s = "";
+    for (int i = 0; i < nums.length; i++) {
+      s = s + nums[i];
+      if (i != nums.length -1) {
+        s += ", ";
+      }
+    }
+    s = "[" + s + "]";
+    return s;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(arrayToString(new int[] {1,2,3}));
+
+  }
+}
