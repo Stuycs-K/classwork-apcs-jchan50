fb469cd779a07c28d494de782723cffbd1c6345a
Jessie Chan <jessiechan55555@gmail.com>
2024-09-27 10:54:29 -0400

    oops last commit message was me testing arrayToString, not MyArrays

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -12,7 +12,7 @@ public class MyArrays{
   }
 
   public static void main(String[] args) {
-    System.out.println(arrayToString(new int[] {1,2,3}));
+    System.out.println(arrayToString(new int[] {1,2,3,4}));
 
   }
 }
