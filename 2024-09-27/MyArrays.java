460658e2e0c4abe879ed613ec97c187f60308c24
Jessie Chan <jessiechan55555@gmail.com>
2024-09-28 20:42:10 -0400

    added testcases for concat

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,3 +1,5 @@
+import javax.swing.text.PlainDocument;
+
 public class MyArrays{
   public static String arrayToString(int[] nums){
     String s = "";
@@ -20,24 +22,45 @@ public class MyArrays{
     return a;
   }
 
-  public static void test(int[] ary) {
+  public static void testReturnCopy(int[] ary) {
     System.out.println("Contents of original array:" + arrayToString(ary));
     System.out.println("Contents of copy:" + arrayToString(returnCopy(ary)));
     System.out.println("Checking if addresses are the same: " + (ary == returnCopy(ary)));
   }
+
+  public static int[] concatArray(int[]ary1,int[]ary2) {
+    return new int[] {};
+  }
+
+  public static void testConcatArray(int[] bry1, int[]bry2) {
+    System.out.println("Components: " + arrayToString(bry1) + " " + arrayToString(bry2));
+    System.out.println(arrayToString(concatArray(bry1, bry2)));
+  }
+  
   public static void main(String[] args) {
     System.out.println(arrayToString(new int[] {1,2,3,4}));
 
     //test cases for returnCopy
     int[] ary1 = {2,3,4};
-    test(ary1);
+    testReturnCopy(ary1);
     int[] ary2 = {2,3,4,3,0};
-    test(ary2);
+    testReturnCopy(ary2);
     int[] ary3 = {};
-    test(ary3);
+    testReturnCopy(ary3);
     int[] ary4 = {2,3,-4};
-    test(ary4);
+    testReturnCopy(ary4);
     int[] ary5 = {2,3,4, 3, 3, 2, 2, 2, 2};
-    test(ary5);
+    testReturnCopy(ary5);
+
+    //test cases for returnCopy
+
+    testConcatArray(new int[] {223432,34,2,0}, new int[] {6,8,6} );
+    testConcatArray(new int[] {2,0,0,0,0,0,0,2}, new int[] {} );
+    testConcatArray(new int[] {}, new int[] {6,8,6} );
+    testConcatArray(new int[] {}, new int[] {} );
+    testConcatArray(new int[] {2}, new int[] {6,8,6} );
+    testConcatArray(new int[] {2}, new int[] {6} );
+    testConcatArray(new int[] {22222}, new int[] {6} );
+    
   }
 }
