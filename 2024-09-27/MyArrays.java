c86ff7c83fe6668d327ef2edf28be6d89bfaa3aa
Jessie Chan <jessiechan55555@gmail.com>
2024-09-28 20:19:50 -0400

    returnCopy passes test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -13,16 +13,31 @@ public class MyArrays{
 
 
   public static int[] returnCopy(int[]ary){
-    return new int[] {0};
+    int[] a = new int[ary.length];
+    for (int i = 0; i < ary.length; i++) {
+      a[i] = ary[i];
+    }
+    return a;
+  }
+
+  public static void test(int[] ary) {
+    System.out.println("Contents of original array:" + arrayToString(ary));
+    System.out.println("Contents of copy:" + arrayToString(returnCopy(ary)));
+    System.out.println("Checking if addresses are the same: " + (ary == returnCopy(ary)));
   }
   public static void main(String[] args) {
     System.out.println(arrayToString(new int[] {1,2,3,4}));
 
     //test cases for returnCopy
     int[] ary1 = {2,3,4};
-    System.out.println("Contents of original array:" + arrayToString(ary1));
-    System.out.println("Contents of copy:" + arrayToString(returnCopy(ary1)));
-    System.out.println("Checking if addresses are the same:" + ary1==returnCopy(ary1));
-
+    test(ary1);
+    int[] ary2 = {2,3,4,3,0};
+    test(ary2);
+    int[] ary3 = {};
+    test(ary3);
+    int[] ary4 = {2,3,-4};
+    test(ary4);
+    int[] ary5 = {2,3,4, 3, 3, 2, 2, 2, 2};
+    test(ary5);
   }
 }
