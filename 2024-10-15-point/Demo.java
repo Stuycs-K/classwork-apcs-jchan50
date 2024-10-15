cd409478c65883b542e0f078bd82a559f071ead1
Jessie Chan <jessiechan55555@gmail.com>
2024-10-15 16:19:34 -0400

    more test cases

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -15,10 +15,19 @@ public class Demo{
     Point p2 = new Point(-1,-1);
     Point p3 = new Point(3,4);
     Point p4 = new Point(p3);
-    System.out.println( p3);
+    System.out.println("this is point 3: " +  p3);
+    System.out.println("this is point 4: " + p4);
+    Point triangle1 = new Point(0,0);
+    Point triangle2 = new Point(1,0);
+    Point triangle3 = new Point(1/2,Math.sqrt(3) / 2);
+
+
     System.out.println( distance(p1,p2));
+    System.out.println( distance(p1,p3));
     System.out.println( Point.distance(p1,p2));
+    System.out.println( Point.distance(p1,p3));
     System.out.println( p1.distanceTo(p2));
-    System.out.println(p4);
+    System.out.println( p1.distanceTo(p3));
+
   }
 }
