f105f511cd6b552db932e32f754157456f5c556e
Jessie Chan <jessiechan55555@gmail.com>
2024-10-15 16:22:27 -0400

    oops forgot to say that we also created the triangle points in class

--- 2024-10-15-point/Demo.java
+++ 2024-10-15-point/Demo.java
@@ -21,7 +21,6 @@ public class Demo{
     Point triangle2 = new Point(1,0);
     Point triangle3 = new Point(1/2,Math.sqrt(3) / 2);
 
-
     System.out.println( distance(p1,p2));
     System.out.println( distance(p1,p3));
     System.out.println( Point.distance(p1,p2));
@@ -29,5 +28,6 @@ public class Demo{
     System.out.println( p1.distanceTo(p2));
     System.out.println( p1.distanceTo(p3));
 
+    System.out.println("");
   }
 }
