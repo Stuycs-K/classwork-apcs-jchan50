16696b81ee129ab5a5c09ea0c04d0b660d4d645c
Jessie Chan <jessiechan55555@gmail.com>
2024-10-31 11:14:09 -0400

    we were testing with w an array. nextLine doesnt let us skip a line

--- 10-31-Advent/ReadFile.java
+++ 10-31-Advent/ReadFile.java
@@ -7,15 +7,31 @@ public class ReadFile {
 
   public static void main(String[] args) {
     //2 Opening a file requires a try/catch
+
+    int i = 0;
+    int[] test = new int[]  {3,4,3,6,32,1,3};
+    while(i < test.length ){
+      if ((i % 2) == 0){
+        System.out.println(test[i]);
+        //do something with the next line of the file
+      }
+      i++;
+    }
+
     try {
       File file = new File("ReadFile.java");//1
       Scanner input = new Scanner(file);
       //CODE THAT SCANS THE FILE.
-      while(input.hasNextLine()){
-    System.out.println(input.nextLine());
-    //do something with the next line of the file
+      int index = 0;
+      while(input.hasNextLine() ){
+        if ((index % 2) == 0){
+          System.out.println(input.nextLine());
+          //do something with the next line of the file
+        }
+        index++;
       }
       input.close();//releases the file from your program
+
 
 
     } catch (FileNotFoundException ex) {
@@ -25,3 +41,4 @@ public class ReadFile {
     }
   }
 }
+//prints out itself
