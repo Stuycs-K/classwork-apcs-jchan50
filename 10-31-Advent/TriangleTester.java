7328077bcdcfc77d8bf60b50b2c087c5f2618f92
Jessie Chan <jessiechan55555@gmail.com>
2024-11-05 21:48:16 -0500

    sorry for the lack of commits i was sick. created helper function isTriangle

---
+++ 10-31-Advent/TriangleTester.java
@@ -0,0 +1,19 @@
+public class TriangleTester{
+    public static int countTrianglesA(String filename){
+        int count = 0;
+        
+    }
+
+    public static boolean isTriangle(int a, int b, int c){
+        if (c > a + b){
+            return false;
+        }
+        if (b > a + c){
+            return false;
+        }
+        if (a > c + b){
+            return false;
+        }
+        return true;
+    }
+}
