bcef20e8df8291050a5238f127a0a83147574221
Jessie Chan <jessiechan55555@gmail.com>
2024-11-05 22:21:58 -0500

    my function wont take a string. why is the function parameter a String? are we supposed to create a scanner object with that string? i cant figure out how to turn a file into a string

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,7 +1,29 @@
+//1 you need all three imports.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+
 public class TriangleTester{
+   
     public static int countTrianglesA(String filename){
+
+        Scanner input = new Scanner(filename);
+
         int count = 0;
-        int 
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        while(input.hasNextInt){
+            a = nextInt();
+            b = nextInt();
+            c = nextInt();
+            if (isTriangle(a,b,c)){
+                count++;
+            }
+        }
+        return count;
     }
 
     public static boolean isTriangle(int a, int b, int c){
@@ -15,5 +37,14 @@ public class TriangleTester{
             return false;
         }
         return true;
+    }
+
+    public static void main(String[] args){
+
+        File file = new File("inputTri.txt");//1
+
+        System.out.println(isTriangle(5, 100, 23));
+        System.out.println(isTriangle(4, 5, 6));
+        System.out.println(countTrianglesA(file));
     }
 }
