e87f4404a041ff236b6f1bde0267efad3a7cc4aa
Jessie Chan <jessiechan55555@gmail.com>
2024-11-06 16:23:37 -0500

    part b done!

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -7,6 +7,15 @@ import java.util.Scanner;
 
 public class TriangleTester{
 
+    public static boolean isTriangle(int a, int b, int c){
+        // if ((c > a + b) || (b > a + c) ||  (a > c + b)){
+        //     return false;
+        // }
+        // return true;
+        return (c < a + b) && (b < a + c) &&  (a < c + b);
+    }
+
+
     public static int countTrianglesA(String filename){
 
         try {
@@ -31,17 +40,37 @@ public class TriangleTester{
           System.out.println("File not found");
           return -1; //you can return from a void function just don't put a value.
         }
-
-
     }
 
-    public static boolean isTriangle(int a, int b, int c){
-        // if ((c > a + b) || (b > a + c) ||  (a > c + b)){
-        //     return false;
-        // }
-        // return true;
-        return (c < a + b) && (b < a + c) &&  (a < c + b);
+    public static int countTrianglesB(String filename){
+      try {
+      File file = new File(filename);//1
+      Scanner s = new Scanner(file);
+      int count = 0;
+      while(s.hasNextInt()){
+        int[] nums = new int[9];
+          for (int i = 0; i < nums.length; i++){
+            nums[i] = s.nextInt();
+          }
+          if (isTriangle(nums[0],nums[3],nums[6])){
+              count++;
+          }
+          if (isTriangle(nums[1],nums[4],nums[7])){
+              count++;
+          }
+          if (isTriangle(nums[2],nums[5],nums[8])){
+              count++;
+          }
+      }
+      return count;
+      }
+      catch (FileNotFoundException ex) {
+        //File not found what should you do?
+        System.out.println("File not found");
+        return -1; //you can return from a void function just don't put a value.
+      }
     }
+
 
     public static void main(String[] args){
 
@@ -50,5 +79,6 @@ public class TriangleTester{
         System.out.println(isTriangle(1, 2, 3));
         System.out.println(isTriangle(3, 2, 3));
         System.out.println(countTrianglesA("inputTri.txt"));
+        System.out.println(countTrianglesB("inputTri.txt"));
     }
 }
