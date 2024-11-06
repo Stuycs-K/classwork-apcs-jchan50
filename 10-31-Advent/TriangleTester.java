06a68a05b3fddb0296fbf0c43e3052980aaa84c8
Jessie Chan <jessiechan55555@gmail.com>
2024-11-06 16:09:28 -0500

    changing scanner syntax

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -6,24 +6,33 @@ import java.util.Scanner;
 
 
 public class TriangleTester{
-   
+
     public static int countTrianglesA(String filename){
 
-        
-
+        try {
+        File file = new File(filename);//1
+        Scanner s = new Scanner(file);
         int count = 0;
         int a = 0;
         int b = 0;
         int c = 0;
-        while(filename.hasNextInt){
-            a = nextInt();
-            b = nextInt();
-            c = nextInt();
+        while(s.hasNextInt()){
+            a = s.nextInt();
+            b = s.nextInt();
+            c = s.nextInt();
             if (isTriangle(a,b,c)){
                 count++;
             }
         }
         return count;
+        }
+        catch (FileNotFoundException ex) {
+          //File not found what should you do?
+          System.out.println("File not found");
+          return -1; //you can return from a void function just don't put a value.
+        }
+
+
     }
 
     public static boolean isTriangle(int a, int b, int c){
@@ -41,12 +50,11 @@ public class TriangleTester{
 
     public static void main(String[] args){
 
-        File file = new File("inputTri.txt");//1
-        Scanner input = new Scanner(file);
-        
+
+
 
         System.out.println(isTriangle(5, 100, 23));
         System.out.println(isTriangle(4, 5, 6));
-        System.out.println(countTrianglesA(input));
+        System.out.println(countTrianglesA("inputTri.txt"));
     }
 }
