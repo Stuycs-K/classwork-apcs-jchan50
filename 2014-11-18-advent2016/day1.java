9c519dfd0530a9ea2b99e89a4b3280b0bff5edbe
Jessie Chan <jessiechan55555@gmail.com>
2024-11-19 10:57:17 -0500

    parse

--- 2014-11-18-advent2016/day1.java
+++ 2014-11-18-advent2016/day1.java
@@ -2,50 +2,49 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class day1{
-  private int x;
-  private int y;
 
-  // public String[] parse(String filename){
-  //   try{
-  //     Scanner input = new Scanner(new File(filename)):
-  //     String line = inf.nextLine();
-  //     return line.split(", ");
-  //   }catch(Exception e){
-  //     System.exit(1);
-  //   }
-  // }
-  // Integer.parseInt
-  //
-  // public static int solve(String[] data){
-  //   int x = 0, y = 0, facing = 0;
-  //   int[][]offset = {
-  //     {0,1}, {1,0}, {0,-1}, {-1,0}
-  //   }
-  //   (facing + length) % length
-  // }
-
-  public static boolean Right(String filename){
-    try {
-    Scanner input = new Scanner(new File(filename));
-    while(input.hasNext()){
-        String step = input.next();
-        System.out.println(step);
-    }
-    return true;
-    }
-    catch (FileNotFoundException ex) {
-      //File not found what should you do?
+  public static String[] parse(String filename){
+    try{
+      Scanner input = new Scanner(new File(filename));
+      String line = input.nextLine();
+      return line.split(", ");
+    }catch(Exception e){
       System.exit(1);
-      return false; //you can return from a void function just don't put a value.
+      return null;
     }
   }
+  //Integer.parseInt
+
+  public static int solve(String[] data){
+    int x = 0, y = 0, facing = 0;
+    int[][]compass = {{0,1}, {1,0}, {0,-1}, {-1,0}}
+    
+    //(facing + length) % length
+  }
+
+  // public static boolean Right(String filename){
+  //   try {
+  //   Scanner input = new Scanner(new File(filename));
+  //   while(input.hasNext()){
+  //       String step = input.next();
+  //       System.out.println(step);
+  //   }
+  //   return true;
+  //   }
+  //   catch (FileNotFoundException ex) {
+  //     //File not found what should you do?
+  //     System.exit(1);
+  //     return false; //you can return from a void function just don't put a value.
+  //   }
+  // }
 
   public static void main(String[] args){
     //String[] data = prase("input.txt"):
     //System.out.println(data.arrayToString);
-    System.out.println(Right("advent3puzzleinput.txt"));
+    System.out.println(Arrays.toString(parse("advent3puzzleinput.txt")));
   }
 
 }
