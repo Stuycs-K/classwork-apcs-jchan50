0a899a13cd8ce1b9f598f03ba5d2ea9fb8376d34
Jessie Chan <jessiechan55555@gmail.com>
2024-11-19 22:18:42 -0500

    got tally of letters working

--- 2014-11-18-advent2016/day4.java
+++ 2014-11-18-advent2016/day4.java
@@ -6,12 +6,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class day4{
+
+
     public static String solve(String filename){
-        ArrayList<String> letters = new ArrayList<String>();
-        ArrayList<Integer> tally = new ArrayList<Integer>();
         try{
           Scanner input = new Scanner(new File(filename));
           while (input.hasNextLine()){
+            ArrayList<String> letters = new ArrayList<String>();
+            ArrayList<Integer> tally = new ArrayList<Integer>();
             String line = input.nextLine();
             String[] data = line.split("-");
             //System.out.println(Arrays.toString(data));
@@ -19,8 +21,20 @@ public class day4{
             for (int i = 0; i < data.length -1; i++){
               encr += data[i]; 
             }
-            System.out.println(encr);
-            
+            String[] enc = encr.split(""); 
+            //ArrayList<String> en = new ArrayList<>(Arrays.asList(enc));
+            //System.out.println(Arrays.toString(enc));
+            for (int i = 0; i < enc.length; i++){
+              if (letters.indexOf(enc[i]) != -1){
+                tally.set(letters.indexOf(enc[i]), tally.get(letters.indexOf(enc[i])) + 1); //increases that letter's  tally by one;
+              }
+              else{
+                letters.add(enc[i]);
+                tally.add(1);
+              }
+            }
+            System.out.println(letters);
+            System.out.println(tally);
           }
           return null;
         }catch(Exception e){
