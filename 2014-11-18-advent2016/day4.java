24affc0bec388736eaf968125361ee43b08bc81e
Jessie Chan <jessiechan55555@gmail.com>
2024-11-19 21:53:49 -0500

    day 4 isolated encrypted names

--- 2014-11-18-advent2016/day4.java
+++ 2014-11-18-advent2016/day4.java
@@ -2,41 +2,24 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class day4{
     public static String solve(String filename){
-
+        ArrayList<String> letters = new ArrayList<String>();
+        ArrayList<Integer> tally = new ArrayList<Integer>();
         try{
           Scanner input = new Scanner(new File(filename));
           while (input.hasNextLine()){
             String line = input.nextLine();
             String[] data = line.split("-");
-            System.out.println(Arrays.toString(data));
-            // for (int i = 0; i < data.length; i++){
-            //     //System.out.println(data[i]);
-            //     if (a > 0 && data[i].equals("U")){ //checking that it doesnt leave bounds
-            //         a--;
-            //         // System.out.println("hi");
-            //         // System.out.println(a);
-            //     }
-            //     if (a < 2 && data[i].equals("D")) {
-            //         a++;
-            //         // System.out.println(a);
-            //         // System.out.println("hi");
-            //     }
-            //     if (b < 2 && data[i].equals("R")){
-            //         b++;
-            //         // System.out.println(b);
-            //         // System.out.println("hi");
-            //     }
-            //     if ( b > 0 && data[i].equals("L")) {
-            //         b--;
-            //         // System.out.println(b);
-            //         // System.out.println("hi");
-            //     }
-            // }
-            
+            //System.out.println(Arrays.toString(data));
+            String encr = "";
+            for (int i = 0; i < data.length -1; i++){
+              encr += data[i]; 
+            }
+            System.out.println(encr);
             
           }
           return null;
