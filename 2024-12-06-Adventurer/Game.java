82c7c0afeba8525c1630b85ef13984baf576d714
Jessie Chan <jessiechan55555@gmail.com>
2024-12-09 16:21:13 -0500

    player now does things when you tell it to, also catches any cases where input is not a command

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+
 public class Game {
   public static void main(String[] args) {
     //do this once
@@ -13,7 +16,7 @@ public class Game {
     System.out.println("Username is: " + userName);
 
     Adventurer player = new Worrier(userName,20);
-    Adventurer enemy = new Worrier("Cronmeal",30);
+    Adventurer enemy = new Worrier("Cronwall",30);
 
     while (player.getHP() > 0 && enemy.getHP() > 0){
       System.out.println(player);
@@ -22,6 +25,32 @@ public class Game {
       System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
       String response  = userInput.nextLine();
       System.out.println(response);
+
+      String[] v = {"a", "attack", "sp", "special", "su", "support", "quit"};
+      List<String> l = Arrays.asList(v);
+
+
+      while (l.indexOf(response) == -1){
+        System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+        response  = userInput.nextLine();
+        System.out.println(response);
+      }
+      if (response.equals("a") || response.equals("attack")){
+        System.out.println(player.attack(enemy));
+      }
+      if (response.equals("sp") || response.equals("special")){
+        System.out.println(player.specialAttack(enemy));
+      }
+      if (response.equals("su") || response.equals("support")){
+        System.out.println(player.support());
+      }
+      if (response.equals("quit")){
+        break;
+      }
+      else{
+
+      }
+
       break;
     }
   }
