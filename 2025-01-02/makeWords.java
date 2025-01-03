d14a1fb29f8c42fa1bd67b1f304385d678cb4e25
Jessie Chan <jessiechan55555@gmail.com>
2025-01-03 11:01:47 -0500

    makewords

---
+++ 2025-01-02/makeWords.java
@@ -0,0 +1,11 @@
+public class makeWords{
+  public static void makeWords(int remainingLetters, String result, String alphabet){
+    if (remainingLetters == 0){
+      System.out.println(result);
+    }
+    else{
+      for ()
+      makeWords(remainingLetters -1; result += alphabet
+    }
+  }
+}
